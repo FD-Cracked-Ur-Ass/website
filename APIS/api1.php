cc15e34ac95568dfeb2b74c7698122fb4fd0ffe1
FD-Cracked-Ur-Ass <114500328+FD-Cracked-Ur-Ass@users.noreply.github.com>
2023-07-01 17:15:29 +0300

    Create api1.php

---
+++ APIS/api1.php
@@ -0,0 +1,11 @@
+<?php
+    if (isset($_GET['file'])) {
+        $filename = $_GET['file'];
+        $file = 'Users/'.$filename;
+        if (is_file($file)) {
+            echo 'HkxAChqhZbqaJkUnOQzVwVeUoBcBdw';
+        } else {
+            echo 'Not Whitelisted!';
+        }
+    }
+?>
